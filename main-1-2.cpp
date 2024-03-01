78787f1cc5f2e0f7b23592b7d73a01ac10ea28fa
Kyle050109 <161389102+Kyle050109@users.noreply.github.com>
2024-03-01 20:57:11 +1030

    Delete main-1-2.cpp

--- main-1-2.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-extern double array_mean(int array[], int n);
-int main(){
-int array[]={1,2,3,4,5};
-int n=sizeof(array)/sizeof(array[0]);
-std::cout << "The mean of the array elements is: " << array_mean(array, n) << std::endl;
-return 0;
-}
