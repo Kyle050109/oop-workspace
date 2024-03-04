600f6b3890e1ab885535985f5fddfb9065994b84
Kyle050109 <161389102+Kyle050109@users.noreply.github.com>
2024-03-04 21:15:55 +1030

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-extern std::string converttoBinary(int decimalNumber);
-int main(){
-    int decimalNumber=10;
-    std::string binaryNumber=converttoBinary(decimalNumber);
-    std::cout << "The binary of " << decimalNumber << " is: " << binaryNumber << std::endl;
-    return 0;
-}
