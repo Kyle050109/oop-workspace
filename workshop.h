12fb2f71158e121d155ec7515988bdce03d0c524
Kyle050109 <161389102+Kyle050109@users.noreply.github.com>
2024-03-12 22:45:20 +1030

    Add files via upload

---
+++ workshop.h
@@ -0,0 +1,28 @@
+    #include <iostream>
+    using namespace std;
+    void changeValue(double* val){
+        *val=42;
+    }
+    void printArray(double* array, int size){
+        for(int i=0;i<size;i++){
+            cout << array[i] << " ";
+        }
+        cout << endl;
+    }
+    double arrayMax(double* array, int size){
+        double max=array[0];
+        for(int i=1;i<size;i++){ 
+            if(array[i]>max){
+                max=array[i];
+            }
+        }
+        return max;
+    }
+    double* dynamicArray(int size, double initialize){
+        double* array=new double[size];
+        for (int i=0;i<size;i++){
+            array[i]=initialize;
+        }
+        return array;
+    }
+    
