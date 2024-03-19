f7a0b11d620235c35a3d31ee3abc9d0cf35d8821
Kyle050109 <161389102+Kyle050109@users.noreply.github.com>
2024-03-19 19:55:52 +1030

    Add files via upload

---
+++ function-4-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+int *readNumbers(){
+    int *numbers=new int[10];
+    for(int i=0;i<10;i++){
+    std::cin >> numbers[i];
+    }
+    return numbers;
+}
+int sumSubarray(int* numbers, int start, int end){
+    int sum=0;
+    for (int i=start;i<end;i++){
+        sum+=numbers[i];
+    }
+    return sum;
+}
+int secondSmallestSum(int* numbers, int length){
+    int smallest=INT8_MAX;
+    int secondSmallest=INT8_MAX;
+    for (int start=0;start<length;start++){
+        for (int end=start+1;end<=length;end++){
+            int array=sumSubarray(numbers,start,end);
+            if (array<smallest){
+                secondSmallest=smallest;
+                smallest=array;
+            }else if(array<secondSmallest&&array!=smallest){
+                secondSmallest=array;
+            }
+        }
+    }
+        return secondSmallest;
+    }
