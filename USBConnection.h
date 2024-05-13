ae93b70cf1667ab9f06fa373fb5fc051e1d4a586
Kyle050109 <161389102+Kyle050109@users.noreply.github.com>
2024-05-13 17:44:34 +0930

    Add files via upload

---
+++ USBConnection.h
@@ -0,0 +1,29 @@
+#ifndef USBCONNECTION_H
+#define USBCONNECTION_H
+#include <stack>
+#include <memory>
+class USBConnection{
+private:
+    static std::stack<int> ids; 
+    int ID;
+    USBConnection(int id) : ID(id) {}
+
+public:
+   
+    ~USBConnection() {
+        ids.push(ID);
+    }
+    static USBConnection* CreateUsbConnection() {
+        if (!ids.empty()) {
+            int id = ids.top();
+            ids.pop();
+            return new USBConnection(id);
+        }
+        return nullptr;
+    }
+    int get_id() const {
+        return ID;
+    }
+};
+std::stack<int> USBConnection::ids = std::stack<int>({3, 2, 1});
+#endif
