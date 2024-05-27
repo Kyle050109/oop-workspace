f1175a59eb8862b458b2dbba29c6a3e264beb618
Kyle050109 <161389102+Kyle050109@users.noreply.github.com>
2024-05-27 17:25:39 +0930

    Add files via upload

---
+++ GameEntity.h
@@ -0,0 +1,25 @@
+#ifndef GAME_ENTITY_H
+#define GAME_ENTITY_H
+
+#include <tuple>
+
+class GameEntity {
+public:
+    GameEntity(int x, int y, char type) : position(x, y), type(type){}
+
+    std::tuple<int, int> getPos(){
+        return position;
+    }
+    
+    virtual ~GameEntity() = default;
+    
+    char getType(){
+        return type;
+    }
+
+protected:
+    std::tuple<int, int> position;
+    char type;
+};
+
+#endif
